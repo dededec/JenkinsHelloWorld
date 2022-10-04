d7e82d5d1ef78d642b7e4f2c057357cd709fb980
dededec <43612628+dededec@users.noreply.github.com>
2022-10-04 14:49:46 +0200

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args) {
-		System.out.print("Se vinieron incluso mas cositas");
+		System.out.print("Se vinieron incluso maaas cositas");
 	}
 }
